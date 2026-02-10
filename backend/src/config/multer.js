e18bb7d09e047e7514c71f958a06454110dae6d3
Abdul Hakeem M K <151653465+hakkimbinqamar123@users.noreply.github.com>
2026-02-10 13:14:37 +0530

    changed

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -1,17 +1,42 @@
+// const multer = require("multer");
+// const path = require("path");
+// const fs = require("fs");
+
+// const uploadPath = "uploads/news";
+
+// // Create folder if it doesn't exist
+// if (!fs.existsSync(uploadPath)) {
+//   fs.mkdirSync(uploadPath, { recursive: true });
+// }
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, uploadPath);
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqueName =
+//       Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     cb(null, uniqueName + path.extname(file.originalname));
+//   }
+// });
+
+// const fileFilter = (req, file, cb) => {
+//   if (file.mimetype.startsWith("image/")) {
+//     cb(null, true);
+//   } else {
+//     cb(new Error("Only image files allowed"), false);
+//   }
+// };
+
+// module.exports = multer({ storage, fileFilter });
+
+
 const multer = require("multer");
 const path = require("path");
-const fs = require("fs");
-
-const uploadPath = "uploads/news";
-
-// Create folder if it doesn't exist
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath, { recursive: true });
-}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadPath);
+    cb(null, "/tmp"); // Vercel writable temp directory
   },
   filename: (req, file, cb) => {
     const uniqueName =
