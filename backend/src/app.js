2858cdfa3a0cdd43f40ddb034fcd6d9600da0fe6
Abdul Hakeem M K <151653465+hakkimbinqamar123@users.noreply.github.com>
2026-02-10 12:26:30 +0530

    changed

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 const app = express()
 
 app.use(cors({
-   origin: ["https://yourfrontenddomain.com" || "*"],
+   origin: process.env.CLIENT_URL,
    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
    allowedHeaders: ["Content-Type", "Authorization"],
    credentials: true
